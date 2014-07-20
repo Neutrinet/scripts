50cd019b654948ad93a463c4a9247ef42ee3afbe
Laurent Peuch <cortex@worlddomination.be>
2014-07-20 04:02:05 +0200

    [enh] write pad content to ouiki

--- write_pad_content_to_meeting_pages.py
+++ write_pad_content_to_meeting_pages.py
@@ -19,10 +19,12 @@ for url in ["http://neutrinet.be" + x["href"] for x in soup.find("div", id="mw-p
     if len(body) > 400:
         continue
     to_handle.append((url, pad_url))
-    break
+    print "Add %s (with target %s)" % (url, pad_url)
 
 if not to_handle:
     sys.exit(0)
+
+print "-------"
 
 # login
 page = browser.get("http://neutrinet.be/index.php?title=Special:UserLogin&returnto=Main+Page")
@@ -32,3 +34,21 @@ form.find("input", id="wpName1")["value"] = ""
 form.find("input", id="wpPassword1")["value"] = ""
 
 browser.submit(form, page.url)
+
+# handle pads
+for url, pad_url in to_handle:
+    page = browser.get(url + "&action=formedit")
+    content = page.soup.textarea.text
+
+    if "quadpad" in pad_url:
+        pad_id = filter(None, pad_url.split("/"))[-1]
+        to_append = urlopen("https://quadpad.lqdn.fr/ep/pad/export/%s/latest?format=txt" % pad_id).read()
+    else:
+        to_append = urlopen(os.path.join(pad_url, "export/txt")).read()
+
+    page.soup.textarea.string = content + "\n\n" + to_append.decode("Utf-8")
+
+    form = page.soup("form")[0]
+    form["action"] = page.url
+    browser.submit(form, page.url)
+    print "Done for %s (from %s)" % (url, pad_url)
