6574c14b80d0d5338d65976c4637ff8183f7993f
Thomas Balthazar <thomas@balthazar.info>
2017-01-02 14:21:37 +0100

    Update script to use CIDRs instead of "manual" IPs

--- ipv4/ipv4.py
+++ ipv4/ipv4.py
@@ -1,6 +1,6 @@
 import psycopg2
 import dotenv
-
+from netaddr import *
 
 def connect():
     return psycopg2.connect(host=dotenv.get('HOSTNAME'),
@@ -9,30 +9,23 @@ def connect():
                             password=dotenv.get('PASSWORD'),
                             dbname=dotenv.get('DATABASE'))
 
-def sorted_ipv4s(ipv4s):
-    return sorted(ipv4s, key=lambda a: int(a.split('.')[-1])) 
-
-def get_existing_ipv4s():
+def ips_in_database():
     conn = connect()
     cur = conn.cursor()
     cur.execute('SELECT address FROM address_pool where "ipVersion"=4 AND address like %s', ('80.67.181.%',))
     records = cur.fetchall()
     cur.close
     conn.close()
-    return sorted_ipv4s(map((lambda r: r[0]), records))
+    return IPSet(map(lambda r: r[0], records))
 
-def list_all_possible_ipv4s():
-    prefix = "80.67.181."
-    return map((lambda a: prefix + str(a)), range(256))
-
-def list_ipv4s_to_add(all, existing, exceptions=None):
-    ipv4s_to_add = set(all) - set(existing)
-    if exceptions is not None:
-        ipv4s_to_add -= set(exceptions)
-    return sorted_ipv4s(list(ipv4s_to_add))
+def ips_summary(set):
+    ips = sorted(IPSet(set).iprange())
+    return "%d addresses (%s..%s)" % (len(ips), ips[0], ips[-1])
 
 def insert_ipv4s(addresses):
-    answer = raw_input("Are you sure you want to insert those IP addresses? (N/y) : ")
+    print 'Those IP addresses will be inserted into the database:'
+    print ', '.join(map(lambda a: str(a), addresses))
+    answer = raw_input("Are you sure? (N/y) : ")
     if answer.lower() != 'y':
         print "Aborting..."
         return
@@ -46,7 +39,7 @@ def insert_ipv4s(addresses):
                         (client_id, address, "ipVersion", enabled, "leasedAt", expiry, netmask, purpose)
                         values (-1, %s, 4, true, '2016-12-25 14:00:00', '2026-12-25 14:00:00', 32,
                         'CLIENT_ASSIGN')'''
-        if cur.execute(sql_statement, (address,)) is not None:
+        if cur.execute(sql_statement, (str(address),)) is not None:
             print "Lastrowid: %d" % cur.lastrowid
             print "Query: %s" % cur.query
             print "Status: %s" % cur.statusmessage
@@ -57,17 +50,14 @@ def insert_ipv4s(addresses):
     conn.close()
 
 dotenv.load()
-all_addresses = list_all_possible_ipv4s()
-existing_addresses = get_existing_ipv4s()
-exceptions = []
-for i in range(0, 16):
-    exceptions.append("80.67.181.%d" % i)
-exceptions.append("80.67.181.128")
-exceptions.append("80.67.181.255")
-addresses_to_add = list_ipv4s_to_add(all_addresses, existing_addresses, exceptions)
+neutrinet_ip_range = IPSet(IPNetwork('80.67.181.0/24'))
+reserved_ip_range = IPSet(IPNetwork('80.67.181.0/28'))
+existing_ip_range = IPSet(IPNetwork('80.67.181.128/25')).union(ips_in_database())
+range_to_add = neutrinet_ip_range - reserved_ip_range - existing_ip_range
 
-print "There are %d possible IPv4 addresses for our range. (%s..%s)" % (len(all_addresses), all_addresses[0], all_addresses[-1])
-print "There are %d existing IPv4 addresses for our range. (%s..%s)" % (len(existing_addresses), existing_addresses[0], existing_addresses[-1])
-print "There are %d IPv4 addresses to add for our range:" % len(addresses_to_add)
-print ', '.join(addresses_to_add)
-insert_ipv4s(addresses_to_add)
+print "Neutrinet IP Range: %s" % ips_summary(neutrinet_ip_range)
+print "Reserved IP Range: %s" % ips_summary(reserved_ip_range)
+print "Existing IP Range: %s" % ips_summary(existing_ip_range)
+print "IP Range to add: %s" % ips_summary(range_to_add)
+
+insert_ipv4s(range_to_add)
